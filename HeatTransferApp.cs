c7a938e56dfcc001b4bf447a5943f172a066a630
NemahAlsayed <91572873+NemahAlsayed@users.noreply.github.com>
2025-06-17 11:49:47 +0000

    Rename project and update solution references

--- HeatTransferApp.cs
+++ HeatTransferApp.cs
@@ -1,4 +1,4 @@
-namespace HeatCanvas  // ✅ Ensure namespace is defined
+namespace HeatCanvas // ✅ Ensure namespace is defined
 {
     using Gtk;
 
--- obj/Debug/net8.0/Grid.AssemblyInfo.cs
+++ obj/Debug/net8.0/Grid.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataGridView")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29020e73ade4fe62437d5409569ad102f7872d6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4e58e485071c9dfc190e01586415ceaebeec0f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataGridView")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataGridView")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
